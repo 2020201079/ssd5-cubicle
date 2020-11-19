0ea8f65a583360047607633e1efa909b7ca6235c
nambigari <varunnambigari.iit@gmail.com>
2020-11-19 14:24:36 +0530

    Merged digital clock

--- cl.js
+++ cl.js
@@ -192,4 +192,26 @@ cup.addEventListener("click",function(){
     cup.setAttribute("visible",false);
   }
 });
+
+AFRAME.registerComponent('clock', {
+  schema: {
+    
+    color: {type: 'color', default: '#0f0'},
+    font: {type: 'string', default: 'monoid'}
+  },
+  init: function () {
+    this.clockEl = document.createElement('a-text');
+    this.el.appendChild(this.clockEl); 
+    this.clockEl.setAttribute('position', this.data.position);
+    this.clockEl.setAttribute('color', this.data.color);
+    this.clockEl.setAttribute('font', this.data.font);
+  },
+  tick: function(){
+    this.clockEl.setAttribute('value', this.getTime());
+  }, 
+  getTime: function() {
+      var d = new Date();
+      return d.toLocaleTimeString();
+  }  
+});
  
